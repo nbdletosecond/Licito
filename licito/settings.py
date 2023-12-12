87c9699ee950d600fd7c20f2cfda3de5f8a69f49
nbdletosecond <ferreiraleopardo089@gmail.com>
2023-12-12 11:56:58 -0300

    Add templates, bids App and Tailwind CDN
    To simple visualize what I need to implement using JS

--- licito/settings.py
+++ licito/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'licito.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
